19ddd1c015d4d30373b2a71db5d65dca9097ff65
assaffmatias <matias.assaff98@gmail.com>
2023-12-19 14:29:37 -0300

    commit

--- Client/src/components/Favorites/Favorites.jsx
+++ Client/src/components/Favorites/Favorites.jsx
@@ -31,19 +31,25 @@ const Favorites = (props) => {
         <div className={style.container}>
             <div className={style.selectContainer}>
                 <Link to='/characters'>
-                    <button className={style.button}> Go Back</button>
+                    <button className={style.button}>
+                        <svg className={style.svgIcon} viewBox="0 0 384 512">
+                            <path
+                                d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"
+                            ></path>
+                        </svg>
+                    </button>
                 </Link>
                 <div>
-                    <select className={style.select} onChange={handleOrder}>
-                        <option value="A">Upward</option>
-                        <option value="D">Falling</option>
-                    </select>
                     <select className={style.select} onChange={handleFilter}>
                         <option value="All">All</option>
                         <option value="Male">Male</option>
                         <option value="Female">Female</option>
                         <option value="Genderless">Genderless</option>
                         <option value="unknown">Unknown</option>
+                    </select>
+                    <select className={style.select} onChange={handleOrder}>
+                        <option value="A">Upward</option>
+                        <option value="D">Falling</option>
                     </select>
                 </div>
             </div>
